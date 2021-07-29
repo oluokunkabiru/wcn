a83092aa0e676a628b391d8bfa471664b1153347
OLUOKUN KABIRU ADESINA <villageboy@villageboy.koadit.com>
2021-07-29 13:47:13 +0100

    add configuration to footer

--- resources/views/users/admin/layouts/footer.blade.php
+++ resources/views/users/admin/layouts/footer.blade.php
@@ -1,3 +1,6 @@
+@php
+$configuration =App\Models\Configuration::where('id', 1)->first();
+@endphp
 </div>
 </div>
 <footer class="footer pt-3">
@@ -5,31 +8,28 @@
         <div class="row align-items-center justify-content-lg-between">
             <div class="col-lg-6 mb-lg-0 mb-4">
                 <div class="copyright text-center text-sm text-muted text-lg-left">
-                    © <script>
-                        document.write(new Date().getFullYear())
-
-                    </script>,
-                    made with <i class="fa fa-heart"></i> by
-                    <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                    for a better web.
+                    &COPY;
+                    {{ date('Y') }}
+                    made <i class="fa fa-heart"></i> by
+                    <a href="https://koadit.com" class="font-weight-bold" target="_blank">KOADIT</a>
+                    {{--  {{ $configuration->name }}  --}}
                 </div>
             </div>
             <div class="col-lg-6">
                 <ul class="nav nav-footer justify-content-center justify-content-lg-end">
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Creative
-                            Tim</a>
+                        <a href="{{ route('ourblogs') }}" class="nav-link text-muted" target="_blank">Blogs</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted"
-                            target="_blank">About Us</a>
+                        <a href="{{ route('nuggetquote') }}" class="nav-link text-muted"
+                            target="_blank">Nuggets</a>
                     </li>
                     <li class="nav-item">
-                        <a href="http://blog.creative-tim.com" class="nav-link text-muted" target="_blank">Blog</a>
+                        <a href="{{ route('gallery') }}" class="nav-link text-muted" target="_blank">Gallery</a>
                     </li>
                     <li class="nav-item">
-                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted"
-                            target="_blank">License</a>
+                        <a href="{{ route('event') }}" class="nav-link pe-0 text-muted"
+                            target="_blank">Event</a>
                     </li>
                 </ul>
             </div>
--- resources/views/users/admin/layouts/header.blade.php
+++ resources/views/users/admin/layouts/header.blade.php
@@ -1,9 +1,12 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-left ms-3" id="sidenav-main">
     <div class="sidenav-header">
+        @php
+            $configuration =App\Models\Configuration::where('id', 1)->first();
+        @endphp
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute right-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand m-0" href="{{ route('index') }}">
-        <img src="{{ asset('assets/images/logo.png') }}" class="navbar-brand-img h-100" alt="...">
-        <span class="ms-1 font-weight-bold">WCN</span>
+        <img src="{{ $configuration->getMedia('logo')->first()->getUrl() }}" class="navbar-brand-img h-100" alt="...">
+        <span class="ms-1 font-weight-bold text-uppercase">{{ $configuration->name }}</span>
       </a>
     </div>
     <hr class="horizontal dark mt-0">
